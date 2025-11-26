fb1105562415c4a38af2ac491313fca3db1b433b
phuthanet <phuthanet@mut.ac.th>
2025-11-26 08:35:54 +0700

    Update

---
+++ LAB2/LAB2_1.py
@@ -0,0 +1,5 @@
+initial_fee=36500000.675
+team="Liverpool "
+rating=7.61
+
+print("Salah signed a long-term contract with {} for an initial $ {} fee. His Rating in Premier League is {}.".format(initial_fee, rating,team))
