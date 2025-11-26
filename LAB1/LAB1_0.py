ba530359d79d7ef8ada9f26f058de4f7d182814c
phutha <phuthanet@mut.ac.th>
2025-11-26 09:45:05 +0700

    Update LAB1

---
+++ LAB1/LAB1_0.py
@@ -0,0 +1,11 @@
+print('[ENCC0201 Computer Programming]')
+print("Hello MUT")
+
+print("I'm a teacher.")
+print('ความรักก็เหมือน "ROV" เพราะ "MVP" มีแค่หนึ่งเดียว')
+
+print("I'm a teacher.",'ความรักก็เหมือน "ROV" เพราะ "MVP" มีแค่หนึ่งเดียว')
+
+print("2+5")
+print("2"+"5")
+print(2+5)
---
+++ LAB1/LAB1_1.py
@@ -0,0 +1,5 @@
+print("6719230001")
+print([ENCC0201 Computer Prgramming])
+    print('Hello\nWorld')
+
+print("My Name is","Phuthanet")
